d118046c7c720ced09f043ddf2379bff13396e82
Utkarsh Deoli <utkarsh.deoli@gmail.com>
2022-03-24 10:17:06 +0530

    ##

---
+++ FunPalin.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int reverse (int a)
+{
+    int sum = 0 ;
+    int temp = a;
+    while (temp >0)
+    {
+        int x = temp%10;
+        sum = (sum*10)+x;
+        temp /=10;
+    }
+
+    
+    return (sum);
+}
+void palin(int a , int b)
+{
+    if (a==b)
+        printf("PalinDrome");
+    else
+        printf("Not PalinDrome");
+}
+void main()
+{
+    int x = 0;
+    scanf("%d", &x);
+    int ans = reverse(x);
+    palin(ans ,x);
+}
